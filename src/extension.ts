ddace63e4d989a61d654074d5322d42db53900c9
Harry Hopkinson <harryjhopkinson@gmail.com>
2022-01-03 21:13:23 +0000

    Created an icon image

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,36 @@
 import * as vscode from 'vscode';
 import "typescript";
 
-export function activate(context: vscode.ExtensionContext) {
-	
-	let disposable = vscode.commands.registerCommand('code-comments.CodeComment', () => {
-		
-	});
+let myStatusBar : vscode.StatusBarItem;
 
-	context.subscriptions.push(disposable);
+export function activate({ subscriptions }: vscode.ExtensionContext) {
+	const codeCommentCommand = "code-comments.CodeComment";
+	subscriptions.push(vscode.commands.registerCommand(codeCommentCommand, () => {
+		vscode.window.activeTextEditor.edit(edit => {
+			let selection = vscode.window.activeTextEditor.selection;
+			let text = vscode.window.activeTextEditor.document.getText(selection);
+			edit.replace(selection, `// ${text}`);
+		});
+	}));
+
+	myStatusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	myStatusBar.command = codeCommentCommand;
+	subscriptions.push(myStatusBar);
+
+	subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
+	subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem));
+	updateStatusBarItem();
+}
+
+function updateStatusBarItem() {
+	if (vscode.window.activeTextEditor) {
+		myStatusBar.text = `$(comment) Comment Code`;		
+		myStatusBar.show();
+	} else {
+		myStatusBar.hide();
+	}
 }
 
 export function deactivate() {
-	
+	myStatusBar.dispose();
 }
